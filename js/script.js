59c929fe10aa94097db800ba4b8f31132dc33b6f
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-14 14:42:25 +0200

    add and print nome e cognome studenti

--- js/script.js
+++ js/script.js
@@ -6,13 +6,34 @@
 
 
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
-var studenti = {
+var studente = {
     nome : 'francesco',
     cognome : 'rossi',
     età : 32
 };
 
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-for (key in studenti) {
-    document.getElementById('output').innerHTML += key + ' : ' + studenti[key] + '<br>';
+for (var key in studente) {
+    document.getElementById('output').innerHTML += key + ' : ' + studente[key] + '<br>';
+}
+
+// Creare un array di oggetti di studenti.
+var studenti = [
+    {
+        nome : 'cristiano',
+        cognome : 'verdi',
+        età : 30
+    },
+    {
+        nome : 'federica',
+        cognome : 'bianchi',
+        età : 22
+    }
+]
+
+// console.log(studenti);
+
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+for (var i = 0; i < studenti.length; i++) {
+    document.getElementById('output2').innerHTML += studenti[i].nome + ' ' + studenti[i].cognome + '<br>';
 }
