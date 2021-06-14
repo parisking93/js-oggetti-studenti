0a0698ad061b81e63e74cfa17f304d881bb3e3d4
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-14 14:27:19 +0200

    add and print oggetto

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
+
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+var studenti = {
+    nome : 'francesco',
+    cognome : 'rossi',
+    età : 32
+};
+
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+for (key in studenti) {
+    document.getElementById('output').innerHTML += key + ' : ' + studenti[key] + '<br>';
+}
